b283f05d6d35d975a5ee6e8e1c5efe9ade41c4b7
Patrick Gerke <ptgerke@gmail.com>
2019-04-26 00:10:08 +0200

    refactor to class in progress, need to implement error handling

--- ffmpeg_python.py
+++ ffmpeg_python.py
@@ -19,6 +19,94 @@ def pad_zero(num, pad):
         num = "0" + num
     return num
 
+class Bin:
+
+    def __init__(self, dir):
+        self.dir = dir
+        self.dir_list = os.listdir(self.dir)
+        self.shot_dict = self.latest_ver_dict()
+        
+
+    @staticmethod
+    def extract_shot_components(shot):
+        se = re.search(r'S\d{2}E\d{2}', shot, re.IGNORECASE)
+        sq = re.search(r'SQ\d{4}', shot, re.IGNORECASE)
+        sh = re.search(r'SH\d{4}', shot, re.IGNORECASE)
+        _ver = re.search(r'v\d{1}', shot, re.IGNORECASE)
+
+
+
+        if _ver:
+            ver_start = shot[_ver.end(0)]
+            ver = ver_start
+            idx = 0
+            # while str.isdigit(shot[ver_start + idx]):
+            #     ver += shot[ver_start + idx]
+            #     idx += 1
+            # ver = int(ver)
+        else:
+            ver = 1
+
+        if not se or not sq or not sh:
+            return
+     
+        else:
+            return {
+                "season": shot[se.start(0)+1:se.start(0)+3],
+                "episode": shot[se.start(0)+4:se.start(0)+6],
+                "sequence": shot[sq.start(0)+2:sq.start(0)+6],
+                "shot": shot[sh.start(0)+2:sh.start(0)+6],
+                "ver": ver
+            }
+
+    
+    def latest_ver_dict(self):
+        _dir = self.dir_list
+        _dict = {}
+        for shot in _dir:
+            shot_dict = self.extract_shot_components(shot)
+            shot_key = f'{shot_dict["season"]}_{shot_dict["episode"]}_{shot_dict["shot"]}'
+            if not shot_key in _dict:
+                _dict[shot_key] = shot_dict
+            if shot_dict["ver"] > _dict[shot_key]["ver"]:
+                _dict[shot_key]["ver"] = shot_dict["ver"]
+        return _dict
+
+
+    # class Shot:
+
+    #     # stream = None
+
+    #     def __init__(self, sea, epi, seq, shot, ver=None):
+    #         self.sea = sea
+    #         self.epi = epi
+    #         self.seq = seq
+    #         self.shot = shot
+    #         if ver:
+    #             self.ver = ver
+    #         else:
+    #             self.ver = 1
+
+    #     @classmethod
+    #     def from_shot_dict(cls, shot_dict):
+    #         sea = shot_dict["season"]
+    #         epi = shot_dict["episode"]
+    #         seq = shot_dict["sequence"]
+    #         shot = shot_dict["shot"]
+    #         ver = shot_dict["ver"]
+
+    #         return cls(sea, epi, seq, shot, ver)
+
+
+bin = Bin(vid_dir)
+# list = bin.latest_shot_ver()
+
+print(bin.shot_dict)
+
+
+
+    
+
 
 def create_shot_string(shot):
     return  f'monster_S{shot["season"]}E{shot["episode"]}_SQ{shot["sequence"]}_SH{shot["shot"]}'
@@ -93,19 +181,19 @@ def create_concat_stream(shot_list):
         input_list.append(ffmpeg.input(shot_path))
     return ffmpeg.concat(*input_list)
 
-concat_stream = (
-    create_concat_stream(
-    create_file_list(
-    find_ult_ver(
-    breakdown_name(
-    vid_dir    
-)))))
+# concat_stream = (
+#     create_concat_stream(
+#     create_file_list(
+#     find_ult_ver(
+#     breakdown_name(
+#     vid_dir    
+# )))))
 
-(
-    concat_stream
-    .output(output)
-    .run()
-)
+# (
+#     concat_stream
+#     .output(output)
+#     .run()
+# )
 
 
 # vid001 = ffmpeg.input(path.join(vid_dir, "Clip002_V002.mp4"))
